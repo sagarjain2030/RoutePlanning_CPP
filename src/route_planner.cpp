dca08c162a68a8ff4d34d66a799a40c73255d3d6
sagarjain2030 <sagarjain2030@gmail.com>
2021-07-30 16:24:20 +0530

    Feat : Final path construction method  Changes to be committed: 	modified:   src/route_planner.cpp

--- src/route_planner.cpp
+++ src/route_planner.cpp
@@ -86,6 +86,21 @@ std::vector<RouteModel::Node> RoutePlanner::ConstructFinalPath(RouteModel::Node 
     std::vector<RouteModel::Node> path_found;
 
     // TODO: Implement your solution here.
+	
+	while(current_node->parent)
+    {
+        distance += current_node->distance(*(current_node->parent));
+        path_found.push_back(*current_node);
+        current_node = current_node->parent;
+    }
+
+    //Checking if current node is start node
+    if(current_node->distance(*start_node) < 1.0)
+    {
+        path_found.push_back(*current_node);
+    }
+
+    reverse(path_found.begin(),path_found.end());	
 
     distance *= m_Model.MetricScale(); // Multiply the distance by the scale of the map to get meters.
     return path_found;
